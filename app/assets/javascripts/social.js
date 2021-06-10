42e0656543f9327a81f98a4f186edce6ccb5f715
Pierre Belzile <pierre.belzile@gmail.com>
2021-06-09 21:34:37 -0400

    upgraded social

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -22,13 +22,13 @@ window.fbAsyncInit = function() {
      /* appId      : '1548975615363971', dev account app */
      appId      : '377502229121424',  /* prod account app */
     xfbml      : false,
-    version    : 'v2.8',
+    version    : 'v3.2',
     cookie     : true
   });
 };
 
 /* helper pour logout de facebook */
-fbLogout = function() {
+var fbLogout = function() {
   FB.getLoginStatus (function (response) {
     if (response.authResponse) {
       FB.logout(function(response) {
@@ -40,48 +40,39 @@ fbLogout = function() {
   });
 };
 
-/* event handler pour google login callback */
-googleSigninCallback = function(authResult) {
-  if (authResult['status']['method'] === 'AUTO') {
-  } else if (authResult['status']['signed_in']) {
-    document.cookie="login=google";
-    
-    /* Delete this member object because it is not needed and causes
-     * a security violation because from another frame */
-    delete authResult['g-oauth-window'];
-    
-    $.post('/auth/google_oauth2/callback', authResult)
-      .done(function(data) {
+var onGoogleSignin = function(googleUser) {
+	document.cookie="login=google";
+	var id_token = googleUser.getAuthResponse().id_token;
+	var access_token = googleUser.getAuthResponse().access_token;
+	$.post('/auth/google_oauth2/callback', {"access_token" : access_token})
+	  .done(function(data) {
+	    $('body').html(data);
+	    socialInitHandlers();
+	  })
+	.fail(function(jqXHR, textStatus) {
+	  alert("Failed auth processing on server. Please contact support. " + textStatus);
+	});
+};
+
+var onGoogleSigninFailure = function() {
+    $.post('/auth/failure', {provider: 'google', msg: authResult['error']}, function(data) {
         $('body').html(data);
         socialInitHandlers();
-      })
-    .fail(function(jqXHR, textStatus) {
-      alert("Failed auth processing on server. Please contact support. " + textStatus);
-    });
-  } else if (authResult['error'] == 'user_signed_out') {
-    window.location = '/signout';
-  } else if (authResult['error'] == 'immediate_failed') {
-    /* Not already log in. Let dialogue take place */
-  } else {
-    $.post('/auth/failure', {provider: 'google', msg: authResult['error']}, function(data) {
-      $('body').html(data);
-      socialInitHandlers();
-    });
-  }
-};  
+      });
+}
 
-socialLogOut = function(event) {
+var socialLogOut = function(event) {
   var scLg = getCookie('login');
   if (scLg == 'fb') {
     fbLogout();
     event.preventDefault();
   } else if (scLg == 'google') {
-    gapi.auth.signOut();
+    gapi.auth2.getAuthInstance().signOut();
   }
 };
 
 
-facebookSignin = function(event) {
+var facebookSignin = function(event) {
   FB.login(function(response) {
     if (response.status === 'connected') {
       document.cookie="login=fb";
@@ -109,20 +100,21 @@ facebookSignin = function(event) {
   event.preventDefault();
 };
 
-googleSignIn = function(event) {
-  var parms = {
-    'clientid' : '1040962372599-s1m9rrdlgdo6bk12u4sjcuq877hvtvk6.apps.googleusercontent.com',
-    'cookiepolicy' : 'single_host_origin',
-    'scope' : 'profile email',
-    'callback' : googleSigninCallback
-  };
-  gapi.auth.signIn(parms);
-  event.preventDefault();
-};
-
-socialInitHandlers = function() {
+var socialInitHandlers = function() {
+  gapi.load('auth2', function() {
+	  var gparms = {
+	    'client_id' : '1040962372599-s1m9rrdlgdo6bk12u4sjcuq877hvtvk6.apps.googleusercontent.com',
+	    'cookiepolicy' : 'single_host_origin',
+	    'scope' : 'profile email',
+	  };
+	  gapi.auth2.init(gparms);
+	  $("#google_signin").click(function(event) {
+			gapi.auth2.getAuthInstance().signIn().then(onGoogleSignin, onGoogleSigninFailure);
+			event.preventDefault();
+	  });
+  });
+  
   $('#facebook_signin').click(facebookSignin);
-  $('#google_signin').click(googleSignIn);
   $('#sign_out').click(socialLogOut);
 };
 
